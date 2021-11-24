24284bf65afd6ec3f0f559d4069dc3332fac9691
Alex Noles <alexnoles@outlook.com>
2021-11-24 19:12:19 +0000

    Player Animation

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private PlayerMovement pm;
+    private SpriteRenderer sr;
+    private bool firstFrameDash;
+    public Sprite normalSprite;
+    public Sprite dashSprite;
+    public Sprite fallSprite;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        pm = GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        if (pm.dashing)
+        {
+            if (firstFrameDash)
+            {
+                sr.sprite = dashSprite;
+                sr.flipY = pm.rb.velocity.x < 0;
+                float targetX = pm.rb.velocity.normalized.x;
+                float targetY = pm.rb.velocity.normalized.y;
+                transform.right = (transform.position + new Vector3(targetX, targetY)) - transform.position;
+                firstFrameDash = false;
+            }
+        }
+        else
+        {
+            firstFrameDash = true;
+            sr.flipX = false;
+            sr.flipY = false;
+            transform.rotation = Quaternion.identity;
+            if(pm.rb.velocity.y == 0)
+            {
+                sr.sprite = normalSprite;
+            }
+            else
+            {
+                sr.sprite = fallSprite;
+            }
+        }
+
+        if (pm.firstFrameMouseNotHeld)
+        {
+            firstFrameDash = true;
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    [HideInInspector]
+    public Rigidbody2D rb;
+    private BoxCollider2D bc2D;
     private Camera mainCamera;
     public bool mouseHeldDown
     {
@@ -14,6 +16,12 @@ public class PlayerMovement : MonoBehaviour
     {
         get { return Input.GetMouseButtonDown(0); }
     }
+
+    public bool firstFrameMouseNotHeld
+    {
+        get { return Input.GetMouseButtonUp(0); }
+    }
+
     private Vector2 mousePosInScreenSpace
     {
         get { return mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraCanvas.planeDistance)); }
@@ -33,7 +41,7 @@ public class PlayerMovement : MonoBehaviour
     //Ground check
     private Vector2 groundCheckPos
     {
-        get { return new Vector2(transform.position.x, transform.position.y) + (Vector2.down * 0.45f); }
+        get { return new Vector2(transform.position.x, transform.position.y) + (Vector2.down * 0.57f) + (Vector2.left * 0.05f); }
     }
     public Vector2 groundCheckExtents = new Vector2(0.2f, 0.5f);
 
@@ -42,11 +50,13 @@ public class PlayerMovement : MonoBehaviour
         get { return Physics2D.OverlapBox(groundCheckPos, groundCheckExtents, 0f, ground); }
     }
 
-    private Vector2 storedMousePos;
     private Vector2 targetDirection = Vector2.zero;
     private Vector2 savedVel;
     private float normalFixedDeltaTime;
-    private bool dashing;
+    [HideInInspector]
+    public bool dashing;
+    private Vector2 defaultColliderSize;
+    public Vector2 dashingColliderSize;
 
     const float horizontalDrag = 0.02f;
     const float gravity = 0.2f;
@@ -62,6 +72,8 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        bc2D = GetComponent<BoxCollider2D>();
+        defaultColliderSize = bc2D.size;
         mainCamera = Camera.main;
         normalFixedDeltaTime = Time.fixedDeltaTime; //0.02
     }
@@ -72,10 +84,6 @@ public class PlayerMovement : MonoBehaviour
         {
             Time.timeScale = slowedDownTimeScale;
             Time.fixedDeltaTime = normalFixedDeltaTime / (1/slowedDownTimeScale);
-            if (firstFrameMouseHeldDown)
-            {
-                storedMousePos = mousePosInScreenSpace;
-            }
             //Find vector between storedMousePos and the current mouse position
             targetDirection = Vector2.ClampMagnitude(mousePosInScreenSpace - new Vector2(transform.position.x,transform.position.y), 5f);
         }
@@ -115,6 +123,8 @@ public class PlayerMovement : MonoBehaviour
     {
         if (!dashing)
         {
+            gameObject.layer = 0;
+            bc2D.size = defaultColliderSize;
             if (!grounded)
             {
                 rb.velocity += (Vector2.down * gravity);
@@ -124,6 +134,11 @@ public class PlayerMovement : MonoBehaviour
                 rb.velocity = new Vector2(0, 0);
             }
             rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, 0, horizontalDirection * horizontalDrag), rb.velocity.y);
+        }
+        else
+        {
+            bc2D.size = dashingColliderSize;
+            gameObject.layer = 9;
         }
     }
 
