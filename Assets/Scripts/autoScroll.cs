4fa1edb4ea21b71503be9218531dd65093f8a81f
Alex Noles <alexnoles@outlook.com>
2021-11-28 20:59:11 +0000

    Add intro screen and intro animation

---
+++ Assets/Scripts/autoScroll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autoScroll : MonoBehaviour
+{
+    public float scrollSpeed = 1.0f;
+    private float imageLength;
+    private Vector3 startPos;
+
+    private void Start()
+    {
+        startPos = transform.position;
+        imageLength = GetComponent<SpriteRenderer>().sprite.bounds.extents.x * 2;
+    }
+
+    private void Update()
+    {
+        transform.position += new Vector3(scrollSpeed * Time.deltaTime,0);
+        if(transform.position.x > startPos.x + imageLength)
+        {
+            transform.position -= new Vector3(imageLength,0);
+        }
+    }
+}
